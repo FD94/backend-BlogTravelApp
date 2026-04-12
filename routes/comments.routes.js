5fe336b188745b43375e5ecb3c0a18095aff3b3c
Gianfranco <gianfranco.devita@hotmail.com>
2026-04-12 12:11:18 +0200

    Fix: fix bug with logic from router.comment. post

--- routes/comments.routes.js
+++ routes/comments.routes.js
@@ -5,15 +5,20 @@ const { isAuthenticated } = require("../middleware/jwt.middleware");
 const Comment = require("../models/Comment.model");
 
 router.post("/comments", isAuthenticated, async (req, res) => {
-	const { text, author, post } = req.body;
+	const { text, post } = req.body;
 
 	try {
 		if (!text) {
 			return res.status(422).json({ message: "El texto es obligatorio" });
 		}
 
-		const newComment = await Comment.create({ text, author, post });
-		const populatedComment = await newComment.populate("author post");
+		const newComment = await Comment.create({
+			text,
+			post,
+			author: req.payload._id,
+		});
+		const populatedComment = await newComment.populate("post author");
+
 		res.status(201).json(populatedComment);
 	} catch (error) {
 		console.log("Error creating comment...", error);
@@ -32,7 +37,7 @@ router.get("/comments/:postId", async (req, res) => {
 	}
 
 	try {
-		const allComments = await Comment.find({ postId })
+		const allComments = await Comment.find({ post: postId })
 			.populate("author")
 			.populate("post");
 
@@ -40,6 +45,32 @@ router.get("/comments/:postId", async (req, res) => {
 	} catch (error) {
 		console.log("Error to getting all comments in this post...", error);
 		res.status(500).json({ Error: "Failed to get all comment in this post" });
+	}
+});
+
+router.delete("/comments/:commentId", isAuthenticated, async (req, res) => {
+	const { commentId } = req.params;
+
+	try {
+		const comment = await Comment.findById(commentId);
+
+		if (!comment) {
+			return res.status(404).json({ message: "Comentario no encontrado" });
+		}
+
+		// 👇 comprobación de dueño
+		if (comment.author.toString() !== req.payload._id) {
+			return res
+				.status(403)
+				.json({ message: "No tienes permiso para borrar este comentario" });
+		}
+
+		await Comment.findByIdAndDelete(commentId);
+
+		res.status(200).json({ message: "Comentario eliminado" });
+	} catch (error) {
+		console.log("Error deleting comment", error);
+		res.status(500).json({ message: "Error deleting comment" });
 	}
 });
 
